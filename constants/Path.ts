c8af18adceb7882338a202bfee43eaa3791b4112
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2022-07-20 15:10:39 +0900

    path.tsの修正

--- constants/Path.ts
+++ constants/Path.ts
@@ -6,8 +6,8 @@ export enum Path {
   BLOG_DETAIL = "/blog_detail",
   CONTACT = "/contact",
   ADMIN = "/admin",
-  ADMIN_DETAIL = "admin_detail",
-  ADMIN_NEW = "admin_new",
-  ADMIN_EDIT = "admin_edit",
-  LOGIN = "login"
+  ADMIN_DETAIL = "/admin_detail",
+  ADMIN_NEW = "/admin_new",
+  ADMIN_EDIT = "/admin_edit",
+  LOGIN = "/login",
 }

e2a215f8b60157b7bae7f002f2fa8555c308008e
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2022-07-25 22:41:21 +0900

    el-upload 調整

--- pages/admin_new/index.vue
+++ pages/admin_new/index.vue
@@ -20,6 +20,7 @@
           :before-upload="handleBefore"
           multiple
           :limit="1"
+          http-request="uploadHttpRequest"
         >
           <i class="el-icon-upload"></i>
           <div class="el-upload__text">
@@ -72,6 +73,9 @@ export default Vue.extend({
       await blogApi.uploadImgData(
         this.fileList
       );
+    },
+    uploadHttpRequest (data) {
+
     },
     onSuccess (res, file) {
       this.fileList = file.raw;
